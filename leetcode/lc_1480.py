433b3c87a4d74b034041cfcf9cebef7533e55148
Manasvi <manasviisonone@gmail.com>
2026-07-06 23:38:46 +0530

    solved lc_1480
    https://leetcode.com/problems/running-sum-of-1d-array/

---
+++ leetcode/lc_1480.py
@@ -0,0 +1,11 @@
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        total = 0
+        ans = []
+
+        for num in nums :
+            total = total + num
+            ans.append(total)
+
+        return ans   
+        
