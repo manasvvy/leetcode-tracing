003e20a81e0f86c617ef6e63ea2e320d71678fae
Manasvi <manasviisonone@gmail.com>
2026-07-11 16:54:38 +0530

    square of sorted array
    leetcode 977

---
+++ leetcode/lc_977.py
@@ -0,0 +1,7 @@
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        ans = []
+        for num in nums:
+            ans.append(num**2)
+            ans.sort()
+        return ans
