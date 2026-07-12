ec29009630f7727f69516c7ba78135973ac6ca23
Manasvi <manasviisonone@gmail.com>
2026-07-12 15:29:43 +0530

    Add solution for defanging an IP address
    leetcode 1108

---
+++ leetcode/lc_1108.py
@@ -0,0 +1,10 @@
+class Solution:
+    def defangIPaddr(self, address: str) -> str:
+        ans = ""
+        for char in address:
+            if char == ".":
+                ans += "[.]"
+            else:
+                ans += char
+
+        return ans          
