14f4d9dbf18b4b05ecbcb387c2134ccceb8ce3ee
Manasvi <manasviisonone@gmail.com>
2026-07-16 22:36:02 +0530

    isSameAfterReversals method

---
+++ leetcode/lc_2119.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isSameAfterReversals(self, num: int) -> bool:
+      
+        if num == 0:
+            return True
+
+        if num % 10 == 0:
+            return False
+
+        return True
