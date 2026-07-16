71ed721c7582b634f94cabccfae90d34cb8b3b92
Manasvi <manasviisonone@gmail.com>
2026-07-16 22:25:30 +0530

    count odd numbers in range

---
+++ leetcode/lc_1523.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countOdds(self, low: int, high: int) -> int:
+        n = high - low + 1
+
+        if n % 2 == 0:
+            return n // 2
+
+        if low % 2 != 0:
+            return n // 2 + 1
+
+        return n // 2
