7b53c4dc1d88e3693947f412d3c664864305341a
Manasvi <manasviisonone@gmail.com>
2026-07-24 12:12:32 +0530

    Implement canConstruct method in lc_383.py

---
+++ leetcode/lc_383.py
@@ -0,0 +1,17 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+
+        count = {}
+        for ch in magazine:
+            if ch in count:
+                count[ch] += 1
+            else:
+                count[ch] = 1
+
+        for ch in ransomNote:
+            if ch not in count or count[ch] == 0:
+                return False
+
+            count[ch] -= 1
+
+        return True
