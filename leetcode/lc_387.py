9dd10421214993b7d3903b122650d0a14c78fd95
Manasvi <manasviisonone@gmail.com>
2026-07-25 22:15:43 +0530

    Add firstUniqChar method to Solution class
    Implement firstUniqChar method to find first unique character in a string.

---
+++ leetcode/lc_387.py
@@ -0,0 +1,14 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        count={}
+        for ch in s:
+            if ch in count:
+                count[ch] += 1
+            else:
+                count[ch] = 1
+
+        for i in range(len(s)):
+            if count[s[i]]==1:
+                return i
+
+        return -1           
