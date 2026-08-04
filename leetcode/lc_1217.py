a19ada5894c3250a6ad65bed002dc608fe6d4608
Manasvi <manasviisonone@gmail.com>
2026-08-04 22:21:12 +0530

    Add minCostToMoveChips function in lc_1217.py
    Implement minCostToMoveChips function to calculate minimum cost based on chip positions.

---
+++ leetcode/lc_1217.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minCostToMoveChips(self, position: List[int]) -> int:
+        odd = 0
+        even = 0
+
+        for pos in position:
+            if pos % 2 == 0:
+                even += 
+            else:
+                odd += 1
+        return min(odd, even)
+        
+
+
+
+
+
+
+
+
+
+
+
+
