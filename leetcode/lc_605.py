7429d5b66753c199d89fa546bbc572ae9a4c081e
Manasvi <manasviisonone@gmail.com>
2026-08-05 21:43:00 +0530

    Implement canPlaceFlowers function in lc_605.py

---
+++ leetcode/lc_605.py
@@ -0,0 +1,15 @@
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+
+        for i in range(len(flowerbed)):
+
+            left_empty = (i == 0 or flowerbed[i - 1] == 0)
+
+            right_empty = (i == len(flowerbed) - 1 or flowerbed[i + 1] == 0)
+
+            if flowerbed[i] == 0 and left_empty and right_empty:
+                n -= 1
+                flowerbed[i] = 1 
+        
+
+        return n <= 0
