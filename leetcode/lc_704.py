3ab4a24c863762ae0e1a652bb12fed0fbc9b8862
Manasvi <manasviisonone@gmail.com>
2026-08-16 21:58:26 +0530

    Update lc_704.py

--- leetcode/lc_704.py
+++ leetcode/lc_704.py
@@ -4,7 +4,7 @@ class Solution:
         right = len(nums) - 1
 
         while left <= right:
-            mid = (left + right) // 2
+            mid = (left + right) // 2 
 
             if nums[mid] == target:
                 return mid
